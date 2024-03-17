755a06d66028820004fea6288a17ca9b26e14e86
Maria Castaneda <mariafcastanedaa@gmail.com>
2024-03-17 18:44:22 -0500

    Se modifica el código de Users.js

---
+++ Users.js
@@ -0,0 +1,46 @@
+class UsersManager {
+    #users = [];
+
+    create(data) {
+        const user = {
+            id: this.#generateId(),
+            name: data.name,
+            photo: data.photo,
+            email: data.email,
+            password: data.password,
+            role: 0,
+        };
+        this.#users.push(user);
+        console.log("El usuario se creo correctamente");
+    }
+
+    read() {
+        return this.#users;
+    }
+
+    #generateId() {
+        if (this.#users.length === 0) {
+            return 1;
+        } else {
+            return this.#users[this.#users.length - 1].id + 1;
+        }
+    }
+}
+
+const userManagerInstance = new UsersManager();
+
+userManagerInstance.create({
+    name: "María",
+    photo: "Maria1.jpg",
+    email: "maria08@hotmail.com",
+    password: "Buendia123",
+});
+
+userManagerInstance.create({
+    name: "Carolina",
+    photo: "Carolina1.jpg",
+    email: "carolina_rocha01@gmail.com",
+    password: "Carorocha08",
+});
+
+console.log(userManagerInstance.read());
