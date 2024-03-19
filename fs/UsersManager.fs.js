f1574a525808d010c21ae34c39a30c6e80c55908
Maria Castaneda <mariafcastanedaa@gmail.com>
2024-03-18 21:38:57 -0500

    Se crea y se ajusta el UsersManager

---
+++ fs/UsersManager.fs.js
@@ -0,0 +1,93 @@
+const fs = require("fs")
+const crypto = require("crypto")
+const { json } = require("stream/consumers")
+
+class UsersManager {
+    constructor() {
+        this.path = "./fs/files/user.json"
+        this.init()
+    }
+    init() {
+        const exists = fs.existsSync(this.path)
+        if (!exists) {
+            const stringData = JSON.stringify([], null, 2)
+            fs.writeFileSync(this.path, stringData)
+            console.log("Archivo creado")
+        } else {
+            console.log("Archivo ya existe")
+        }
+    }
+
+    async create(data) {
+        const user = {
+            id: crypto.randomBytes(12).toString("hex"),
+            name: data.name,
+            photo: data.photo,
+            email: data.email,
+            password: data.password,
+            role: data.role,
+        };
+
+        if (!data.email || !data.password || !data.role) { console.log("Usuario no creado, por favor ingrese todos los datos.") }
+        else {
+            let users = await fs.promises.readFile(this.path, "utf-8")
+            users = JSON.parse(users)
+            users.push(user);
+            console.log("El usuario se creo correctamente");
+           users= JSON.stringify(users, null, 2)
+            await fs.promises.writeFile(this.path, users)
+        }
+    }
+
+    async read() {
+        let users = await fs.promises.readFile(this.path, "utf-8")
+        users = JSON.parse(users)
+        return users;
+    }
+    async readOne(id) {
+        let users = await fs.promises.readFile(this.path, "utf-8")
+        users = JSON.parse(users)
+        return users.find(each => each.id === id)
+    }
+
+    async destroy(id) {
+        let users = await fs.promises.readFile(this.path, "utf-8")
+        users = JSON.parse(users)
+        const filtered = users.filter(each => each.id !== id);
+        await fs.promises.writeFile(filtered)
+        console.log((id + "eliminado"));
+    }
+}
+
+async function test() {
+    const gestorDeUsuarios = new UsersManager();
+    await gestorDeUsuarios.create({
+        name: "María",
+        photo: "Maria1.jpg",
+        email: "maria08@hotmail.com",
+        password: "Buendia123",
+        role: "Analista SEO",
+    });
+    await gestorDeUsuarios.create({
+        name: "Carolina",
+        photo: "Carolina1.jpg",
+        email: "carolina_rocha01@gmail.com",
+        password: "Carorocha08",
+        role: "Marketing director",
+    });
+    await gestorDeUsuarios.create({
+        name: "Pedro",
+        photo: "Pedroperfil.jpg",
+        email: "pedropascual@gmail.com",
+        password: "pedro2007",
+        role: "Head director",
+    });
+    await gestorDeUsuarios.create({
+        name: "Mariana",
+        photo: "Mpedraza.jpg",
+        email: "Mpedraza09@gmail.com",
+        password: "Lichos08",
+        role: "Redactora SEO",
+    });
+}
+test ()
---
+++ memory/UsersManager.memory.js
@@ -0,0 +1,69 @@
+//María Fernanda Castañeda Álvarez
+const crypto = require("crypto")
+
+class UsersManager {
+    static #users = [];
+
+    create(data) {
+        const user = {
+            id: crypto.randomBytes(12).toString("hex"),
+            name: data.name,
+            photo: data.photo,
+            email: data.email,
+            password: data.password,
+            role: data.role,
+        };
+
+        if (!data.email || !data.password || !data.role) 
+       { console.log("Usuario no creado, por favor ingrese todos los datos.") }
+      else {
+            UsersManager.#users.push(user);
+            console.log("El usuario se creo correctamente");
+        }
+    }
+
+    read() {
+        return UsersManager.#users;
+    }
+    readOne(id){
+        return UsersManager.#users.find(each=>each.id===id)
+        //agregar trycatch y condicional en caso de no encontrar usuario
+    }
+
+    destroy(id){
+        const filtered=UsersManager.#users.filter(each=>each.id!==id);
+        UsersManager.#users =filtered;
+        console.log((id+"eliminado"));
+        //agregar trycatch y condicional en caso de no encontrar usuarios
+    }
+}
+const gestorDeUsuarios = new UsersManager();
+gestorDeUsuarios.create({
+    name: "María",
+    photo: "Maria1.jpg",
+    email: "maria08@hotmail.com",
+    password: "Buendia123",
+    role: "Analista SEO",
+});
+gestorDeUsuarios.create({
+        name: "Carolina",
+        photo: "Carolina1.jpg",
+        email: "carolina_rocha01@gmail.com",
+        password: "Carorocha08",
+        role: "Marketing director",
+    });
+    gestorDeUsuarios.create({
+        name: "Pedro",
+        photo: "Pedroperfil.jpg",
+        email: "pedropascual@gmail.com",
+        password: "pedro2007",
+        role: "Head director",
+    });
+    gestorDeUsuarios.create({
+        name: "Mariana",
+        photo: "Mpedraza.jpg",
+        email: "Mpedraza09@gmail.com",
+        password: "Lichos08",
+        role: "Redactora SEO",
+    });
+    console.log(gestorDeUsuarios.read());
