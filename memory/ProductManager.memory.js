d5b7be5322e4ac7fa45685e160c01fa49abf27b2
Maria Castaneda <mariafcastanedaa@gmail.com>
2024-03-24 20:44:54 -0500

    Se hace adición de productos en la memoria de ProductManager

--- memory/ProductManager.memory.js
+++ memory/ProductManager.memory.js
@@ -69,4 +69,41 @@ productManagerInstance.create({
     price: 900000,
     stock: 15
 });
+
+productManagerInstance.create({
+    title: "Paper Towns",
+    photo: "PaperT.jpg",
+    category: "Juvenil",
+    price: 57000,
+    stock: 30
+});
+
+productManagerInstance.create({
+    title: "El Diario de Ana Frank",
+    photo: "EDDAF.jpg",
+    category: "Historia",
+    price: 68000,
+    stock: 10
+});
+productManagerInstance.create({
+    title: "Game Of Thrones",
+    photo: "GOT.jpg",
+    category: "Ficción",
+    price: 98000,
+    stock: 12
+});
+productManagerInstance.create({
+    title: "The 100",
+    photo: "T100.jpg",
+    category: "Ficción",
+    price: 63000,
+    stock: 9
+});
+productManagerInstance.create({
+    title: "Our Common Future",
+    photo: "OCF.jpg",
+    category: "Biologia",
+    price: 110000,
+    stock: 24
+});
 console.log(productManagerInstance.read());
